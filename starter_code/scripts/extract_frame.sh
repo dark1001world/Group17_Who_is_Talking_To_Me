a66ecd4858a37b1f02f30a103b57420ab7313adc
dark1001world <akshat.ron007@gmail.com>
2026-04-05 15:33:30 +0530

    Nothing

--- starter_code/scripts/extract_frame.sh
+++ starter_code/scripts/extract_frame.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-mkdir -p data/video_imgs
-
-for file in /DATA/G17/ego4d_data/v2/clips/*.mp4; do
+for file in /DATA/G17/ego4d_data/v2/v2/clips/*.mp4; do
     name=$(basename "$file" .mp4)
-    out_dir="data/video_imgs/$name"
+    out_dir="/DATA/G17/Data/video/$name"
 
     # ✅ Skip if already processed
     if [ -d "$out_dir" ] && [ "$(ls -A $out_dir 2>/dev/null)" ]; then
--- starter_code/scripts/extract_wave.sh
+++ starter_code/scripts/extract_wave.sh
@@ -1,9 +1,7 @@
-if [ ! -d "data/wave"  ];then
-	mkdir "data/wave"
-fi
-for f in `ls /DATA/G17/ego4d_data/v2/clips/`
+
+for f in `ls /DATA/G17/ego4d_data/v2/v2/clips/`
 do
     echo ${f%.*}
-    ffmpeg -y -i /DATA/G17/ego4d_data/v2/clips/${f} -qscale:a 0 -ac 1 -vn -threads 6 -ar 16000 data/wave/${f%.*}.wav -loglevel panic
+    ffmpeg -y -i /DATA/G17/ego4d_data/v2/v2/clips/${f} -qscale:a 0 -ac 1 -vn -threads 6 -ar 16000 /DATA/G17/Data/wave/${f%.*}.wav -loglevel panic
 done
 
