ec2aeec4d5ff669fd36d2cb30a89fa0cd3b67ae1
Mahak Agarwal <mahakag2805@gmail.com>
2026-05-21 17:33:42 +0530

    cleaned branch

--- MY_WORK(DONT_TOUCH)/ego4d_speaker_attribution_final/src/losses/smoothness_loss.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def temporal_smoothness_loss(probs, track_mask):
-    diff = torch.abs(probs[:, 1:] - probs[:, :-1])
-    mask = track_mask[:, 1:] & track_mask[:, :-1]
-    if mask.sum() > 0:
-        return diff[mask].mean()
-    return torch.tensor(0.0, device=probs.device)
--- MY_WORK(DONT_TOUCH)/ego4d_speaker_attribution_final/src/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .builder import build_benchmark_model
-
-__all__ = ["build_benchmark_model"]
